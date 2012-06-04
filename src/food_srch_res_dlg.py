e2d301c11051f426d3280c03a95e0ef8ca01ea9a
Jason Self <jself@gnu.org>
2012-06-04 10:05:21 -0700

    Updating copyright statements based on previous commits

--- src/food_srch_res_dlg.py
+++ src/food_srch_res_dlg.py
@@ -1,6 +1,6 @@
 #  GNUtrition - a nutrition and diet analysis program.
 #  Copyright(C) 2000-2002 Edgar Denny (edenny@skyweb.net)
-#  Copyright (C) 2010 Free Software Foundation, Inc.
+#  Copyright (C) 2010 2012 Free Software Foundation, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
